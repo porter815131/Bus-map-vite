c37ad0d1cd06a6072a58be9e3bc3c5eb2ad9766c
Jerry Chein <breakingkyo@gmail.com>
2022-02-10 00:51:54 +0800

    updated gitignore

--- src/api/index.js
+++
@@ -1,9 +0,0 @@
-import { fetchData } from './fetchData';
-
-const CITY_ROUTES_URL = 'https://ptx.transportdata.tw/MOTC/v2/Bus/Route/City/';
-
-export const getCityRoutes = (city = '') => {
-  fetchData(`${CITY_ROUTES_URL}${city}`).then(data => {
-    return data;
-  });
-};
